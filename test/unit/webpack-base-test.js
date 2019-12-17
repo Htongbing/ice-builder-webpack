26ed2074d9d1d8917ed4d3670aeb4565e175bc8a
Htongbing <1521855115@qq.com>
2019-12-17 20:19:23 +0800

    feat: update ci

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,6 +4,6 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
 
   it ('entry', () => {
-    assert.equal(baseConfig.entry.index.inclues('/test/smoke/template/src/pages/index/index.js'), true)
+    assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/pages/index/index.js') > -1, true)
   })
 })
